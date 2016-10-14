7df50f9174b5f3f8a1efd56ff185f1b4a7a97d24
Mason Salmon <Salmonmasons@aol.com>
2016-10-14 19:11:00 -0400

    Add files via upload

---
+++ Day_21_Generics.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+// Add your code here
+template<typename T>void printArray(vector<T> a)
+{
+    for(T i:a)cout<<i<<endl;
+}
+int main() {
+
+    vector<int> vInt{1, 2, 3};
+    vector<string> vString{"Hello", "World"};
+
+    printArray<int>(vInt);
+    printArray<string>(vString);
+
+    return 0;
+}
