d203c73e7d06550de8b6a234d787c9f34a1d3e73
Mason Salmon <Salmonmasons@aol.com>
2016-10-15 14:11:54 -0400

    Add files via upload

---
+++ Day_22_Binary_Search_Tree.cpp
@@ -0,0 +1,75 @@
+#include <iostream>
+#include <cstddef>
+
+using namespace std;
+
+class Node{
+    public:
+        int data;
+        Node *left;
+        Node *right;
+        Node(int d){
+            data = d;
+            left = NULL;
+            right = NULL;
+        }
+};
+class Solution{
+    public:
+  		Node* insert(Node* root, int data)
+      {
+            if(root == NULL) {
+                return new Node(data);
+            }
+            else {
+                Node* cur;
+                if(data <= root->data){
+                    cur = insert(root->left, data);
+                    root->left = cur;
+                }
+                else{
+                    cur = insert(root->right, data);
+                    root->right = cur;
+               }
+
+               return root;
+           }
+        }
+    int getHeight(Node* root)
+    {
+        int heightLeft = 0;
+        int heightRight = 0;
+
+        //Write your code here
+
+        if(root->left!=NULL)
+        {
+            heightLeft = getHeight(root->left)+1;
+        }
+        if(root->right!=NULL)
+        {
+            heightRight = getHeight(root->right)+1;
+        }
+        return (heightLeft > heightRight ? heightLeft : heightRight);
+    }:
+
+
+
+
+    int main() {
+        Solution myTree;
+        Node* root = NULL;
+        int t;
+        int data;
+
+        cin >> t;
+
+        while(t-- > 0){
+            cin >> data;
+            root = myTree.insert(root, data);
+        }
+        int height = myTree.getHeight(root);
+        cout << height;
+
+        return 0;
+    }
